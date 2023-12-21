9b75a23fa43ce550ae83d8aa8741c26a84a7cf2b
Nouran Mohsen <nouran.mohsen167@gmail.com>
2023-12-21 23:40:49 +0200

    tasks for week2

--- week2/task7/task7.c
+++ week2/task7/task7.c
@@ -1,32 +1,5 @@
 #include <stdio.h>
-/*
-int main()
-{
-	int N,M;
-	scanf("%d %d",&N,&M);
-	while(N>0&&M>0)
-	{
-		int sum=0;
-		if(N<M)
-		{
-		for(int i=N;i<=M;i++)
-		{
-			printf("%d ",i);
-			sum+=i;
-		}
-	    }
-	else
-	{
-		for(int i=M;i<=N;i++)
-		{
-			printf("%d ",i);
-			sum+=i;
-		}
-	}
-	printf("sum =%d\n",sum);
-	scanf("%d %d",&N,&M);
-    }
-}*/
+
 
 int main() 
 {
